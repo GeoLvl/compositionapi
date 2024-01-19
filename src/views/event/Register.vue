a4e3c69bddce2feb068fa9a7b679a245be70d4d5
NyanBot <laville.geoffrey@gmail.com>
2024-01-19 01:22:48 +0100

    Add gstore

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,21 +1,26 @@
 <script setup>
-// import { defineProps } from "vue";
+import { defineProps } from "vue";
 import { useRouter } from "vue-router";
+import { inject } from "vue";
 
-const router = useRouter();
-
-// defineProps(["event"]);
 const props = defineProps(["event"]);
+const router = useRouter();
+const GStore = inject("GStore");
+
 const register = () => {
-  // if registration API call is successful
+  // Call to API
+  // If registered then redirect to event details
+  GStore.flashMessage =
+    "You are successfully registered for " + props.event.title;
+  setTimeout(() => {
+    GStore.flashMessage = "";
+  }, 3000);
   router.push({
     name: "EventDetails",
-    // pas obligé, populated by default (id)
-    id: props.event.id,
   });
 };
 </script>
 <template>
   <p>Register for the event here</p>
-  <button @click="register">Register me !</button>
+  <button @click="register">Register Me</button>
 </template>
